ea06908469cc52d28f46917b24c244f06ded9b27
Larissa Troper <lmtroper@uwaterloo.ca>
2024-04-24 22:38:44 -0400

    firebase config

--- src/authentication/firebase.js
+++ src/authentication/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbKQwlmY_JazZsoIKy1K1wINIkfHesC4E",
-  authDomain: "clubhub-app-46bd8.firebaseapp.com",
-  projectId: "clubhub-app-46bd8",
-  storageBucket: "clubhub-app-46bd8.appspot.com",
-  messagingSenderId: "272102174754",
-  appId: "1:272102174754:web:275dd077fcc84d9b331a7b",
-  measurementId: "G-SJYN1EGZTC"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
